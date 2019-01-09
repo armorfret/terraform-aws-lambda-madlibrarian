cdfff6480d31fd0d64f1f31ce493bf27aca64a24
Les Aker <me@lesaker.org>
2019-01-08 20:38:07 -0500

    fix apigw lambda uri

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "aws_api_gateway_integration" "integration" {
   http_method             = "${aws_api_gateway_method.method.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.madlibrarian_lambda.invoke_arn}"
+  uri                     = "${module.lambda.invoke_arn}"
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
