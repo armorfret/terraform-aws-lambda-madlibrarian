9f69a3cabc83c3342e9ffab5a5b47e03bd69ef42
Les Aker <me@lesaker.org>
2019-01-08 20:35:07 -0500

    remove apigw permission

--- main.tf
+++ main.tf
@@ -17,14 +17,8 @@ module "lambda" {
 
   access-policy-document = "${data.aws_iam_policy_document.lambda_perms.json}"
   trust-policy-document  = "${data.aws_iam_policy_document.lambda_assume.json}"
-}
 
-resource "aws_lambda_permission" "allow_api_gateway" {
-  function_name = "${aws_lambda_function.madlibrarian_lambda.function_name}"
-  statement_id  = "AllowExecutionFromApiGateway"
-  action        = "lambda:InvokeFunction"
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*"
+  source-arns = ["arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*"]
 }
 
 data "aws_caller_identity" "current" {}
