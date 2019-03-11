f0a3570ea318bd07a76c28a66040f38b1abfeefc
Les Aker <me@lesaker.org>
2019-03-11 23:52:54 +0300

    update version

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "version" {
   description = "Version of the Lambda to use"
   type        = "string"
-  default     = "v0.12.1"
+  default     = "v0.12.2"
 }
 
 variable "logging_bucket" {
